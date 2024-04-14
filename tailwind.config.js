350ac94e26658d561e2705c343ca362f1ccdc3b7
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2024-04-14 04:42:24 -0300

    Configurações Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// Configuration tailwind to the project works well
+
+// In js (index.js) and html (index.html) for example
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
